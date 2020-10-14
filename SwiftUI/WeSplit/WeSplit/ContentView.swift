3816b8fb2b93a7515a60f87f5a19d2b36ea85ab8
Paul Yi <yipaulc@gmail.com>
2020-10-14 12:13:36 -0600

    Complete challenges (see comments)

--- SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
    @State private var checkAmount = ""
-   @State private var numberOfPeople = 2
+   @State private var numberOfPeople = ""
    @State private var tipPercentage = 2
    let tipPercentages = [10, 15, 20, 25, 0]
    var totalPerPerson: Double {
-      let peopleCount = Double(numberOfPeople + 2)
+      // 3. Default peopleCount to 1 or 2 instead of 0 when conversion to Double fails
+      let peopleCount = Double(numberOfPeople) ?? 1
       let tipSelection = Double(tipPercentages[tipPercentage])
       let orderAmount = Double(checkAmount) ?? 0
       
@@ -24,6 +25,15 @@ struct ContentView: View {
       
       return amountPerPerson
    }
+   var checkTotal: Double {
+      let tipSelection = Double(tipPercentages[tipPercentage])
+      let orderAmount = Double(checkAmount) ?? 0
+      
+      let tipValue = orderAmount / 100 * tipSelection
+      let grandTotal = orderAmount + tipValue
+      
+      return grandTotal
+   }
    var body: some View {
       NavigationView {
          Form {
@@ -31,30 +41,27 @@ struct ContentView: View {
                TextField("Amount", text: $checkAmount)
                   .keyboardType(.decimalPad)
                
-               Picker("Number of people", selection: $numberOfPeople) {
-                  ForEach(2 ..< 100) {
-                     Text("\($0) people")
-                  }
-               }
+               TextField("Number of people", text: $numberOfPeople)
             }
-            Section {
+            
+            Section(header: Text("Tip percentage")) {
                Picker("Tip percentage", selection: $tipPercentage) {
                   ForEach(0 ..< tipPercentages.count) {
                      Text("\(self.tipPercentages[$0])%")
                   }
                }.pickerStyle(SegmentedPickerStyle())
             }
-            Section(header: Text("How much tip do you want to leave?")) {
-               ForEach(0 ..< tipPercentages.count) {
-                  Text("\(self.tipPercentages[$0])%")
-               }
-            }.pickerStyle(SegmentedPickerStyle())
-            Section {
+            // 1. Add a header to the third section, saying "Amount per person"
+            Section(header: Text("Amount per person")) {
                Text("$\(totalPerPerson, specifier: "%.2f")")
             }
+            // 2. Add another section showing the total amount for the check - i.e., the original amount plus tip value, without dividing by the number of people
+            Section(header: Text("Check Total (including tip)")) {
+               Text("$\(checkTotal, specifier: "%.2f")")
+            }
          }
+         .navigationBarTitle("WeSplit")
       }
-      .navigationBarTitle("WeSplit")
    }
 }
 
