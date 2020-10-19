a72933b1dddb113c060ca2deb23118c6e0f36b6f
Paul Yi <yipaulc@gmail.com>
2020-10-19 17:10:52 -0600

    Complete all challenges for Project 1

--- UIKit/Project1/Project1/ViewController.swift
+++ UIKit/Project1/Project1/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UITableViewController {
       for item in items {
          if item.hasPrefix("nssl") {
             pictures.append(item)
+            // Challenge: In your main table view, show the image names in sorted order, so "nssl033.jpg" comes before "nssl10034.jpg".
+            pictures.sort()
          }
       }
       print(pictures)
@@ -44,6 +46,9 @@ class ViewController: UITableViewController {
       if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
          // 2. success! Set its selectedImage property
          vc.selectedImage = pictures[indexPath.row]
+         // Challenge: Rather than show image names in the detail title bar, show "Picture X of Y", where Y is the total number of images and X is the selected picture's position in the array. Make sure you count from 1 rather than 0.
+         vc.selectedPictureNumber = indexPath.row + 1
+         vc.totalPictures = pictures.count
          
          //3. now push it onto the navigation controller
          navigationController?.pushViewController(vc, animated: true)
