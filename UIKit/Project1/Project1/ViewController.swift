91631bef7b77f332416bd4d33ce71f80cb4d2072
Paul Yi <yipaulc@gmail.com>
2020-10-18 22:48:16 -0600

    List images with FileManager

---
+++ UIKit/Project1/Project1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Paul Yi on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   var pictures = [String]()
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      let fm = FileManager.default
+      let path = Bundle.main.resourcePath!
+      let items = try! fm.contentsOfDirectory(atPath: path)
+      
+      for item in items {
+         if item.hasPrefix("nssl") {
+            pictures.append(item)
+         }
+      }
+      print(pictures)
+   }
+
+
+}
+
