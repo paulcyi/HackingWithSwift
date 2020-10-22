4633780266724eba8e0bdbe55be17ae82cb1f021
Paul Yi <yipaulc@gmail.com>
2020-10-21 18:10:59 -0600

    Stack up buttons

---
+++ SwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Paul Yi on 10/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        ZStack {
+            Color.blue.edgesIgnoringSafeArea(.all)
+                .foregroundColor(.white)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        // flag was tapped
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
