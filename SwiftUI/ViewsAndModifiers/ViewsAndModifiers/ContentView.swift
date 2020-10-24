8e31e1c419de9cce7710e23a3a8babed0b765a6c
Paul Yi <yipaulc@gmail.com>
2020-10-23 23:24:59 -0600

    Add project

--- SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ SwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useRedText = false
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("St. Luke's")
+            .titleStyle()
+    }
+}
+
+// 1. Create a custom ViewModifier (and accompanying View extension) that makes a view have a large, blue font suitable for prominent titles in a view.
+struct Title: ViewModifier {
+    func body (content: Content) -> some View {
+        content
+            .font(.system(size: 45, weight: .heavy, design: .default))
+            .foregroundColor(.blue)
             .padding()
+            .background(Color.gray)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
     }
 }
 
@@ -19,3 +39,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
