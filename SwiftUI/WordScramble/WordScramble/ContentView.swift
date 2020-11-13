f9a00c26168c5f2c643372e549db88346005a12b
Paul Yi <yipaulc@gmail.com>
2020-11-12 22:50:29 -0700

    Add to a list of words

---
+++ SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Paul Yi on 11/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+        
+        // extra validation to come
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
