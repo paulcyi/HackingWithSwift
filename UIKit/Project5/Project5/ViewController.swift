1c281b4aaac7e8a80233b0137567d533478b809e
Paul Yi <yipaulc@gmail.com>
2020-11-19 23:31:26 -0700

    Set up project

---
+++ UIKit/Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Paul Yi on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
