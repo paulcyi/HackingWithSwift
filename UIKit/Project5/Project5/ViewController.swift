e2c211d1dd29ab5f081e66ac7e597beb66e3769a
Paul Yi <yipaulc@gmail.com>
2020-11-22 20:30:24 -0700

    Prepare for submission: lowercased() and IndexPath

--- UIKit/Project5/Project5/ViewController.swift
+++ UIKit/Project5/Project5/ViewController.swift
@@ -48,8 +48,31 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
+    func isPossible(word: String) -> Bool {
+        return true
+    }
+
+    func isOriginal(word: String) -> Bool {
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        return true
+    }
+    
     func submit(_ answer: String) {
-        
+        let lowerAnswer = answer.lowercased()
+
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
